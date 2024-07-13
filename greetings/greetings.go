ae38829b8107befca6a8ed5a9d717a9357ff045b
ShadowFax <55938330+ShadowFax1731@users.noreply.github.com>
2024-07-14 01:37:43 +0530

    Added unit tests

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -14,6 +14,7 @@ func Hello(name string) (string, error) {
     }
     // Create a message using a random format.
     message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprint(randomFormat()) this line is just to fail the unit test
     return message, nil
 }
 
